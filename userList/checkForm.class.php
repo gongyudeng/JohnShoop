52dcab8d4d54261090da64838e9b50a5f5cc2e18
gongyudeng <gongyudeng@163.com>
2017-08-22 16:53:29 +0800

    11

--- userList/checkForm.class.php
+++ userList/checkForm.class.php
@@ -3,18 +3,28 @@
 * 验证用户提交表单数据
 */
 class checkForm{
-	private $data;
-	function __construct($arr)
+	private $data = array();
+	function __construct($data)
 	{
-		$this->data = $arr;
+		$this->data = $data;
 	}
-	public functtion check($arr){
+	public functtion check(&$arr){
 		if(!(filter_input(INPUT_POST,'username',FILTER_CALLBACK,array('options' =>'checkForm::validate_str')))){
 			$errors['username'] = "用户名不合法";
 		}
 		if(!(filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL))){
 			$errors['email'] = '邮箱格式不正确';
 		}
+		if(!(filter_input(INPUT_POST,'url',FILTER_VALIDATE_URL))){
+			$errors['url'] = '博客地址不合法';
+		}
+		if(!(filter_input(INPUT_POST,'content',FILTER_CALLBACK,array('options'=>'checkForm::validate_str')))){
+			$errors['content'] = '内容不合法';
+		}
+		if(!empty($errors)){
+			
+		}
+
 	}
 	private static function validate_str($str){
 		if(mb_strlen($str,'utf8')<1){
